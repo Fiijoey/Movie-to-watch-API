02df38deba7d9c736eca608ebc0873a979e1427b
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-04-04 03:37:27 +0900

    check swagger.js

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.use('/', require('./swagger'));
+
+router.use('/users', require('./users'));
+
+router.use('/movies', require('./movies'));
+
+router.use('/reviews', require('./reviews'));
+
+router.use('/watchlists', require('./watchlists'));
+
+router.get('/login', passport.authenticate('github'), (req,res)=>{});
+
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if (err) {return next(err);}
+        res.redirect('/');
+    });
+        
+       
+});
+
+
+
+module.exports = router;
