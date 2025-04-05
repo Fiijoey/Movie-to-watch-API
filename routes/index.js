6cfc8ef73a959e3b91ac7ff49561f3a4139c24a8
Fiijoey <kofimensah3730@gmail.com>
2025-04-05 08:54:49 +0000

    feat: update welcome message for the Movie to Watch API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, and welcome to the Movie to Watch API!");
 });
 
 module.exports = router;
